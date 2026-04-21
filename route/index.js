42f2ff3e205bed7204ab83d6d9cd38605c5b6ced
Kazi Nigar Sultana Zinuk <zinuk758@gmail.com>
2026-04-21 15:05:34 +0600

    updated user profile

--- route/index.js
+++ route/index.js
@@ -1,13 +1,17 @@
 const express = require("express");
-const router = express.Router()
-const authRoute = require("./authRoute")
+const router = express.Router();
+const authRoute = require("./authRoute");
+const projectRoute = require("./projectRoute");
+const { authMiddleware } = require("../middleware/authMiddleware");
 
 router.get("/", (req, res) => {
-    res.status(200).send("Hello From Server")
-})
+  res.status(200).send("Hello From Server");
+});
 
-router.use("/auth", authRoute)
+router.use("/auth", authRoute);
+
+router.use("/project", authMiddleware, projectRoute);
 
 module.exports = router;
 
-// mongodb+srv://task-manager:bOotFo8eDI4ZV4sw@cluster0.hppyt.mongodb.net/taskManager-node2601?appName=Cluster0
+// mongodb+srv://node2601-task-manager:bOotFo8eDI4ZV4sw@cluster0.hppyt.mongodb.net/taskManager-node2601?appName=Cluster0
